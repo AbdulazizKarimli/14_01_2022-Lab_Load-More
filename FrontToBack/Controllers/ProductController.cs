38d77ca4f32f6e29db4516df94552c26894fc414
Abdulaziz930 <abdulazizak@code.edu.az>
2022-01-14 13:04:34 +0400

    initial commit

---
+++ FrontToBack/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FrontToBack.Models;
+
+namespace FrontToBack.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly int _productsCount;
+
+        public ProductController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _productsCount = _dbContext.Products.Count();
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Products = _productsCount;
+
+            var products = _dbContext.Products.Include(x => x.Category)
+                .Where(x => x.IsDeleted == false && x.Category.IsDeleted == false).OrderByDescending(x => x.Id).Take(8).ToList();
+            return View(products);
+        }
+
+        public IActionResult Load(int skip)
+        {
+            if (skip >= _productsCount)
+                return BadRequest();
+
+            var products = _dbContext.Products.Include(x => x.Category).OrderByDescending(x => x.Id).Skip(skip).Take(8).ToList();
+
+            return PartialView("_ProductPartial", products);
+        }
+    }
+}
